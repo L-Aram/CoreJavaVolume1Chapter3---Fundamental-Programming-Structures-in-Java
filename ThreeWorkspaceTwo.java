55120713b5c5f229b02b51af03c4471d5d657f63
Aram Escudero <94578563+L-Aram@users.noreply.github.com>
2021-11-25 18:08:43 -0700

    Add files via upload

---
+++ ThreeWorkspaceTwo.java
@@ -0,0 +1,11 @@
+public class ThreeWorkspaceTwo{
+
+    public static void main(String[] args){
+
+        // We can use class constants from other classes if declared as "public"
+        System.out.println("Value CM_PER_INCH from class \"ThreeWorkspaceOne.java\": " + ThreeWorkspaceOne.CM_PER_INCH + "\n\n");
+
+    }
+
+
+}
